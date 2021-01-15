707b766691fc182860340322a1a34ed2a9663983
Austin <villasanamallariaustin@gmail.com>
2021-01-15 19:52:07 +0800

    flixmedia added movie, tv movels

--- flixmedia/models.py
+++ flixmedia/models.py
@@ -1,28 +1,55 @@
 from django.db import models
 from uuid import uuid4
 
-class Flix(models.Model):
+class ID(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     tmdb_id = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+class Flix(models.Model):
     title = models.CharField(max_length=255)
     tmdb_release_date = models.DateField()
-    flix_release_date = models.DateField()
+    flix_release_date = models.DateTimeField(auto_add_now=True)
+
+    class Meta:
+        abstract = True
 
 class Media(models.Model):
-    genres = None
+    genres = models.ManyToManyField('Genre')
 
     class Meta:
         abstract = True
         ordering = ['flix_release_date']
 
-class Movie(Media):
+class Genre(ID):
+    name = models.CharField(max_length=255)
+    GENRE_FOR_CHOICES = [
+        ('mv', 'movie'),
+        ('tv', 'tv'),
+    ]
+    genre_for = models.CharField(
+        max_length = 2,
+        choices = GENRE_FOR_CHOICES,
+    )
+
+class Movie(ID, Flix, Media):
     pass
 
-class TV(Media):
+class TV(ID, Flix, Media):
     pass
 
-class Season(Media):
+class Season(ID, Flix):
+    tv = models.ForeignKey("TV", on_delete=models.CASCADE, related_name="seasons")
     season_number = models.IntegerField()
 
-class Episode(Media):
-    pass
+    class Meta:
+        ordering = ['season_number']
+
+class Episode(ID, Flix):
+    season = models.ForeignKey("Season", on_delete=models.CASCADE, related_name="episodes")
+    episode_number = models.IntegerField()
+
+    class Meta:
+        ordering = ['episode_number']
