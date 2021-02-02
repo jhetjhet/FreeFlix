473cf33004b0ff7739100f2fc3caf1a344635dfe
jhetjhet <villasanamallariaustin@gmail.com>
2021-02-02 14:37:42 +0800

    created node-media-server

--- freeflix-react/src/App.js
+++ freeflix-react/src/App.js
@@ -3,6 +3,10 @@ const App = () => {
   return (
     <div>
       <h1>HELLO WORLD</h1>
+      <form>
+        <label for="file-input">Select File: </label>
+        <input id="file-input" type="file"></input>
+      </form>
     </div>
   );
 }
---
+++ freeflix-react/src/components/FileUploader.js
@@ -0,0 +1,7 @@
+import {useState} from "react-dom"
+
+const FileUploader = () => {
+    return (
+        <h1>HELLO</h1>
+    );
+}
