144c7be0d7669a976b3a07f51c9f4fb82375123e
jhetjhet <villasanamallariaustin@gmail.com>
2021-02-03 12:34:25 +0800

    react semi working upload

--- freeflix-react/src/App.js
+++ freeflix-react/src/App.js
@@ -1,12 +1,30 @@
+import React, {react, useState} from 'react';
+import FileUploader from './components/FileUploader';
+
 const App = () => {
+  const [file, setFile] = useState(null);
+
+  const onFileSelect = (e) => {
+    setFile(e.target.files[0]);
+  }
 
   return (
     <div>
       <h1>HELLO WORLD</h1>
       <form>
-        <label for="file-input">Select File: </label>
-        <input id="file-input" type="file"></input>
+        <label htmlFor="file-input">Select File: </label>
+        <input onChange={onFileSelect} id="file-input" type="file"></input>
       </form>
+      {file && (
+          <FileUploader file={file}>
+            {({bytesUploaded, pause, setPause}) => (
+              <React.Fragment>
+                <h1>UPLOADED= {(bytesUploaded / file.size) * 100} %</h1>
+                <button onClick={e => setPause(!pause)}>{pause ? '||' : '>>'}</button>
+              </React.Fragment>
+            )}
+          </FileUploader>
+        )}
     </div>
   );
 }
--- freeflix-react/src/components/FileUploader.js
+++ freeflix-react/src/components/FileUploader.js
@@ -1,7 +1,63 @@
-import {useState} from "react-dom"
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+// import cookie from 'react-'
 
-const FileUploader = () => {
-    return (
-        <h1>HELLO</h1>
-    );
+const defaultChunkSize = 1024 * 1024;
+
+const stringToHash = (str) => {
+    var hash, chr;
+    for(var i = 0; i < str.length; i++){
+        chr = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0;
+    }
+    return hash;
 }
+
+const FileUploader = ({children, file, onFinish}) => {
+    // upload
+    const [fileChunk, setFileChunk] = useState(null);
+    const [startChunkByte, setStartChunkByte] = useState(undefined);
+    const [endChunkByte, setEndChunkByte] = useState(undefined);
+    const [bytesUploaded, setBytesUploaded] = useState(0);
+    const [totalFileSize, setTotalFileSize] = useState(undefined);
+
+    // state
+    const [pause, setPause] = useState(true);
+    const [cancel, setCancel] = useState(false);
+
+    useEffect(() => {
+        if(!totalFileSize){
+            setTotalFileSize(file.size);
+            setStartChunkByte(0);
+            setEndChunkByte(defaultChunkSize);
+            // console.log(`${file.name}-${file.lastModified}-${file.size}`);
+        }else{
+            if(!pause && !cancel){
+                const remainSize = totalFileSize - bytesUploaded;
+                console.log(remainSize);
+                if(remainSize > 0){
+                    prepareChunk();
+                }
+            }
+        }
+    }, [bytesUploaded, pause, cancel, totalFileSize, file]);
+
+    const prepareChunk = () => {
+        const remainSize = totalFileSize - bytesUploaded;
+        const start = totalFileSize - remainSize;
+        const end = start + Math.min(defaultChunkSize, remainSize);
+        const chunk = file.slice(start, end);
+        uploadChunk(chunk);
+    }
+
+    const uploadChunk = (chunk) => {
+        setTimeout(() => {
+            setBytesUploaded(bytesUploaded + chunk.size);
+        }, 1000);
+    }
+
+    return children({bytesUploaded, pause, setPause});
+}
+
+export default FileUploader;
