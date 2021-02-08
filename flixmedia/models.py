f451ddefa160b1b18575734d0e492759f55be801
jhetjhet <villasanamallariaustin@gmail.com>
2021-02-08 21:46:38 +0800

    update model make episode/season number unique

--- flixmedia/models.py
+++ flixmedia/models.py
@@ -8,23 +8,23 @@ class ID(models.Model):
     class Meta:
         abstract = True
 
-class Flix(models.Model):
+class Flix(ID):
     title = models.CharField(max_length=255)
-    tmdb_release_date = models.DateField()
-    flix_release_date = models.DateTimeField(auto_now_add=True)
+    release_date = models.DateField()
+    date_uploaded = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
+        ordering = ['date_uploaded']
 
-class Media(models.Model):
+class Media(Flix):
     genres = models.ManyToManyField('Genre')
 
-    class Meta:
+    class Meta (Flix.Meta):
         abstract = True
-        ordering = ['flix_release_date']
 
 class Genre(ID):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     GENRE_FOR_CHOICES = [
         ('mv', 'movie'),
         ('tv', 'tv'),
@@ -34,22 +34,30 @@ class Genre(ID):
         choices = GENRE_FOR_CHOICES,
     )
 
-class Movie(ID, Flix, Media):
-    pass
+class Movie(Media):
 
-class TV(ID, Flix, Media):
-    pass
+    class Meta (Media.Meta):
+        constraints = [
+            models.UniqueConstraint(fields=['id', 'genres'], name='movie_id_genre_unique_constraint'),
+        ]
 
-class Season(ID, Flix):
+class TV(Media):
+    
+    class Meta (Media.Meta):
+        constraints = [
+            models.UniqueConstraint(fields=['id', 'genres'], name='tv_id_genre_unique_constraint'),
+        ]
+
+class Season(Flix):
     tv = models.ForeignKey("TV", on_delete=models.CASCADE, related_name="seasons")
-    season_number = models.IntegerField()
+    season_number = models.IntegerField(unique=True)
 
     class Meta:
         ordering = ['season_number']
 
-class Episode(ID, Flix):
+class Episode(Flix):
     season = models.ForeignKey("Season", on_delete=models.CASCADE, related_name="episodes")
-    episode_number = models.IntegerField()
+    episode_number = models.IntegerField(unique=True)
 
     class Meta:
         ordering = ['episode_number']
