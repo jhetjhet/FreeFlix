28200926d52eeddc3a8dce491fefc18788c33ad9
jhetjhet <villasanamallariaustin@gmail.com>
2021-02-17 15:24:43 +0800

    add flixmed and flixfeed models to admin site

--- flixmedia/admin.py
+++ flixmedia/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Genre,
+    Movie,
+    TV,
+    Season,
+    Episode,
+)
+
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(TV)
+admin.site.register(Season)
+admin.site.register(Episode)
