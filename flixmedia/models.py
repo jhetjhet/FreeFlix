2ff780ee6cb7867024de4cafe0455dc9c1bc71b6
jhetjhet <villasanamallariaustin@gmail.com>
2021-02-18 17:07:24 +0800

    update Video abstract model add move and copy option

--- flixmedia/models.py
+++ flixmedia/models.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.db import models
+from django.core.files import File
 from uuid import uuid4
 from flixfeed.models import Rate
 from pathlib import Path
 import re
 import os
+import shutil
+import logging
 
 INVALID_FILE_CHARS = re.compile(r'[\\/:*?"<>|\s]+')
 
@@ -34,17 +37,46 @@ class Media(Flix):
         abstract = True
 
 class Video(models.Model):
-    save_to = None
+    backup_name_extenstion = "backup"
     video = models.FileField(null=True, default=None)
 
-    def link_local_video(self, vid_path):
+    """
+    Delete video object and rename its file temporarily and delete it
+    after successfully saving of file.
+    In case an error occur during the process the original file will not
+    get overwriten and re asign it as the video file.
+    """
+    def __backup_and_delete_vid__(self):
+        if not self.video or not os.path.exists(self.video.path):
+            return None
+        old_vid_path = f"{self.video.path}.{self.backup_name_extenstion}"
+        if os.path.exists(old_vid_path):
+            os.remove(old_vid_path)
+        os.rename(self.video.path, old_vid_path)
+        self.video.delete(save=False)
+        return old_vid_path
+
+    def link_local_video(self, vid_path, move=False, save=True):
+        old_video_path = self.__backup_and_delete_vid__()
+
         _, filename = os.path.split(vid_path)
         vid_name = filename if not self.save_to else self.save_to(filename)
         vid_path_root = os.path.join(settings.MEDIA_ROOT, vid_name)
         Path(os.path.split(vid_path_root)[0]).mkdir(parents=True, exist_ok=True)
-        os.rename(vid_path, vid_path_root)
+
+        try:
+            getattr(shutil, 'move' if move else 'copyfile')(vid_path, vid_path_root)
+            if old_video_path:
+                os.remove(old_video_path)
+        except (Exception, KeyboardInterrupt):
+            print("An error occur reseting operation...")
+            if os.path.exists(vid_path_root):
+                os.remove(vid_path_root)
+            os.rename(old_video_path, vid_path_root)
+
         self.video.name = vid_name
-        self.save()
+        if save:
+            self.save()
 
     class Meta:
         abstract = True
